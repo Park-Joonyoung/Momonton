089c62ea8983995f95d4096c4ea56e43aba16e7d
Park Joonyoung <sparkjy18@gmail.com>
2020-06-10 01:40:36 +0900

    Add files via upload

---
+++ greeting.js
@@ -0,0 +1,45 @@
+const formGreeting = document.querySelector(".js-form-greeting"),
+  inputGreeting = formGreeting.querySelector("input"),
+  greeting = document.querySelector(".js-greeting");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = inputGreeting.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  formGreeting.classList.add(SHOWING_CN);
+  formGreeting.addEventListener("submit", handleSubmit);
+}
+
+function paintGreeting(text) {
+  formGreeting.classList.remove(SHOWING_CN);
+  greeting.classList.add(SHOWING_CN);
+  greeting.innerText = `Hello, ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+  if (currentUser === null) {
+    // she is not
+    askForName();
+  } else {
+    //she is
+    paintGreeting(currentUser);
+  }
+}
+
+function init() {
+  loadName();
+}
+
+init();
